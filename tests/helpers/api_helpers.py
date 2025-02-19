19fb6269a94322f02fb489bd2f809b4a09ee6a72
Max <maksym.polynskyi@coro.net>
2025-02-19 12:08:51 +0100

    make test_websocket_performance use async httpx client instead of sync requests client

--- tests/helpers/api_helpers.py
+++ tests/helpers/api_helpers.py
@@ -1,8 +1,24 @@
 from requests import Session, Response
 from typing import Optional
+import httpx
 
 
-def create_order(client: Session, stoks: str = "EURUSD", quantity: float = 100.5, custom_order_data: Optional[dict] = None) -> Response:
+async def async_create_order(httpx_async_client: httpx.AsyncClient, stoks: str = "EURUSD", quantity: float = 100.5,
+                             custom_order_data: Optional[dict] = None) -> Response:
+    """
+        :param httpx_async_client: httpx client with set up base url
+        :param stoks: str; ex EURUSD
+        :param quantity: int; ex 100.5
+        :param custom_order_data: dict with custom data to send it on /ORDERS. used for negative testing; ex {"wrong key": "something", "wrongkey2": "something2"}
+        :return: requests Response obj
+        """
+    order_data = custom_order_data if custom_order_data else {"stoks": stoks, "quantity": quantity}
+    response = await httpx_async_client.post("/orders", json=order_data, timeout=10)
+    return response
+
+
+def create_order(client: Session, stoks: str = "EURUSD", quantity: float = 100.5,
+                 custom_order_data: Optional[dict] = None) -> Response:
     """
     :param client: requests client with set up base url
     :param stoks: str; ex EURUSD
